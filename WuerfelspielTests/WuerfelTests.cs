95a0a184b03b9b33a7c99ac8cb511ead435c20ca
Samet <samet.t@gso.schule.koeln>
2022-03-30 12:49:28 +0200

    test

--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -8,11 +8,30 @@ namespace WuerfelspielTests
     public class WuerfelTests
     {
         [TestMethod]
-        public void DummyTest()
+        public void Wuerfel_KonstruktorTest()
         {
-            // Dieser Test ist hier, um eine GitHub-Funktion zu testen. 
-            // Einfach löschen :)
-            Assert.IsTrue(true);// is tatsächlich true :O
+            //Arrange
+            int anzahlSeiten = 6;
+            Wuerfel w = new Wuerfel(anzahlSeiten);
+            //Act
+          
+            //Assert
+            Assert.AreEqual(6, w.anzahlSeiten)
+                //push test
+        }
+        [TestMethod]
+        public void LetztesErgebnisTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+        [TestMethod]
+        public void SicherungUmschaltenTest()
+        {
+
         }
     }
 }
